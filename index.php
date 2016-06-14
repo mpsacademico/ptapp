98aaa94c645f4a9a001114ab3f08a5d3496798e9
Marcos Paulo da Silva <mpsacademico@gmail.com>
2016-06-14 16:13:51 -0300

    Captura de dados de previsão do XML

--- index.php
+++ index.php
@@ -6,8 +6,7 @@
 		<link rel="stylesheet" href="">
 		<script src="_js/jquery-2.2.4.min.js"></script>
 	    <script type="text/javascript">
-		    $(document).ready(function(){
-		    	console.log("documento pronto");			       
+		    $(document).ready(function(){		    				       
 		        $.ajax({
 		        	method: 'GET',
 		        	url: '244.xml',
@@ -28,27 +27,45 @@
 		        		var umidade = metar.find('umidade').text();
 		        		var dirvento = metar.find('dirvento').text();
 		        		var velvento = metar.find('velvento').text();
-		        		var pressao = metar.find(pressao);		        		
-		        		$("#cidade").text(nome);
-		        		$("#uf").text(uf);
-		        		console.log(pais);
-		        		console.log(atualizacao);
-		        		console.log(data);
+		        		var pressao = metar.find('pressao').text();
+		        		var uvatual	= atual.find('uvatual');
+		        		var indice = uvatual.find('indice').text();
+		        		var datauv = uvatual.find('datauv').text();	        		
+		        		$(this).find('previsao').each(function(){
+		        			var dia = $(this).find('dia').text();
+		        			var data = $(this).find('data').text();
+		        			var ico = $(this).find('ico').text();
+		        			var desc = $(this).find('desc').text();
+		        			var texto = $(this).find('texto').text();
+		        			var min = $(this).find('min').text();
+		        			var max = $(this).find('max').text();
+		        			var prob = $(this).find('prob').text();
+		        			var sunrise = $(this).find('sunrise').text();
+		        			var sunset = $(this).find('sunset').text();
+		        			var uv = $(this).find('uv').text();
+		        			var modelo = $(this).find('modelo');
+		        			var h03 = modelo.find('h03').text();
+		        			var h09 = modelo.find('h09').text();
+		        			var h15 = modelo.find('h15').text();
+		        			var h21 = modelo.find('h21').text();
+		        			var str = "<article><p><img src=\"_img/icones/"+ico+".png\"/></p><p>"+dia+"</p><p>Mín. "+min+"ºC | Máx. "+max+"ºC</p><p>Prob. de Chuva: "+prob+"</p><p>UV: "+uv+"</p><p>"+texto+"</p></article>";		  
+		        			$('#previsao').append(str);      			
+		        		}); 						
 		        	});
-
-		        });
-		        
+		        });		        
 		    });
     	</script>
 	</head>
 	<body>
-		<section id="previsao-atual">
+		<!--<section id="previsao-atual">
 			<div id="cidade">Cidade</div>
 			<div uf="uf">UF</div>
 			<div id="dia-semana">Dia-semana</div>
 			<div id="data">DD/MM/AAAA</div>
 			<div id="imagem">ícone</div>
 			<div id="descricao">descrição</div>
+		</section>-->
+		<section id="previsao">
 		</section>
 	</body>
 </html>
